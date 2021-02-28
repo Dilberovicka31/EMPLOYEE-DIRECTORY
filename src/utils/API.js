d0f0cf30e02b06ffd1a889265b888eea85301b6b
Dilberovicka31 <dilberovicm36@gmail.com>
2021-02-27 18:19:18 -0800

    added components and utils folder

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/";
+// eslint-disable-next-line import/no-anonymous-default-export
+const API = {
+
+  search: function(numUsers) {
+    return axios.get(BASEURL`results=${numUsers}`);
+  }
+};
+
+export default API;
